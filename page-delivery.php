e5b00988625b4efeaac0668d8050e9b23a93cb43
Reza Firmansyah <erzethones.inc@gmail.com>
2026-03-16 10:40:43 +0800

    feat: Implement location-based tabs for delivery options and refactor platform display with dynamic styling.

--- page-delivery.php
+++ page-delivery.php
@@ -10,8 +10,8 @@
 get_header();
 ?>
 <main id="primary">
-  <?php get_template_part( 'template-parts/content', 'header' ); ?>
-	
+  <?php get_template_part('template-parts/content', 'header'); ?>
+
   <div class="container-fluid my-5">
     <div class="row">
       <div class="col-12 text-center text-intro">
@@ -23,95 +23,158 @@ get_header();
         </div>
       <?php endif; ?>
     </div>
-  
-    <?php if( have_rows('image_list') ): ?>
-    <div class="position-relative">
-      <div class="container">
-        <div class="row ">
-		<!--GloriaFood-->
-          <div class="col-12 col-lg-4 g-1 g-lg-5">
-            <div class="bg-dark position-relative delivery-section-height">
-              <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
-                <div id="Delivery_Gloria_Food" class="p-3 p-lg-5">
-				  <span data-glf-cuid="9e72489a-49f1-496d-a753-c535e7babe5b" data-glf-ruid="196905ff-9f7c-47fb-83ca-d08ba5e042f1" >
-                  <div class="d-block delivery-trigger">
-                  <img src="<?php bloginfo('template_url'); ?>/images/home-delivery.png" alt="Home Delivery" img-client> 
+
+    <div class="row mb-5">
+      <div class="col-12">
+        <style>
+          .delivery-tabs .nav-link {
+            color: #000;
+            background-color: #fff;
+            border-color: #dee2e6 !important;
+            transition: all 0.3s ease;
+          }
+
+          .delivery-tabs .nav-link.active {
+            background-color: #b40304 !important;
+            color: #fff !important;
+            border-color: #b40304 !important;
+          }
+
+          /* Grab & Gojek Green Colors */
+          .bg-grabfood {
+            background-color: #00b14f !important;
+          }
+
+          .bg-gofood {
+            background-color: #00aa13 !important;
+          }
+
+          .bg-grabfood .img-cover,
+          .bg-gofood .img-cover {
+            opacity: 1;
+          }
+        </style>
+        <ul class="nav nav-pills justify-content-center delivery-tabs mb-5" id="deliveryTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active px-5 py-3 fw-bold text-uppercase rounded-0 border" id="ubud-tab"
+              data-bs-toggle="pill" data-bs-target="#ubud" type="button" role="tab" aria-controls="ubud"
+              aria-selected="true">UBUD</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link px-5 py-3 fw-bold text-uppercase rounded-0 border" id="sanur-tab"
+              data-bs-toggle="pill" data-bs-target="#sanur" type="button" role="tab" aria-controls="sanur"
+              aria-selected="false">SANUR</button>
+          </li>
+        </ul>
+
+        <div class="tab-content" id="deliveryTabContent">
+          <!-- Ubud Tab -->
+          <div class="tab-pane fade show active" id="ubud" role="tabpanel" aria-labelledby="ubud-tab">
+            <div class="container">
+              <div class="row g-1 g-lg-5">
+                <!-- GloriaFood / Home Delivery khusus Ubud -->
+                <div class="col-12 col-lg-4">
+                  <div class="bg-dark position-relative delivery-section-height">
+                    <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
+                      <div id="Delivery_Gloria_Food" class="p-3 p-lg-5">
+                        <span data-glf-cuid="9e72489a-49f1-496d-a753-c535e7babe5b"
+                          data-glf-ruid="196905ff-9f7c-47fb-83ca-d08ba5e042f1">
+                          <div class="d-block delivery-trigger" style="cursor:pointer;">
+                            <img src="<?php bloginfo('template_url'); ?>/images/home-delivery.png" alt="Home Delivery"
+                              img-client>
+                          </div>
+                        </span>
+                      </div>
+                    </div>
+                    <img src="<?php bloginfo('template_url'); ?>/images/delivery-background.jpg" class="img-cover">
                   </div>
-                </span>
                 </div>
-              </div>
-              <img src="<?php bloginfo('template_url'); ?>/images/delivery-background.jpg" class="img-cover">
-            </div>          
-          </div>
-		<!--Grab Food-->
-		  <div class="col-12 col-lg-4 g-1 g-lg-5">
-            <div class="bg-dark position-relative delivery-section-height">
-              <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
-                <div class="p-3 p-lg-5">
-                  <a rel="nofollow" id="grabfood" href="https://grab.onelink.me/2695613898?pid=inappsharing&c=6-C2WZC2LFE4BGEA&is_retargeting=true&af_dp=grab%3A%2F%2Fopen%3FscreenType%3DGRABFOOD%26sourceID%3DA4pcqCZkS4%26merchantIDs%3D6-C2WZC2LFE4BGEA&af_force_deeplink=true&af_web_dp=https%3A%2F%2Fwww.grab.com%2Fdownload" class="text-white stretched-link delivery-trigger" target="_blank" >  
-                    <div class="d-block">
-                     <img src="https://www.goldenmonkeyubud.com/wp-content/uploads/2021/11/grabfood-150.png" alt="Grab Food" target="_blank" img-client> 
+
+                <?php if (have_rows('image_list')): ?>
+                  <?php while (have_rows('image_list')):
+                    the_row();
+                    $banner_id = get_sub_field('banner_id');
+                    $bg_class = 'bg-dark';
+                    if (stripos($banner_id, 'grab') !== false) {
+                      $bg_class = 'bg-grabfood';
+                    } elseif (stripos($banner_id, 'go') !== false) {
+                      $bg_class = 'bg-gofood';
+                    }
+
+                    // Get custom background image
+                    $custom_bg = get_sub_field('banner_background_image');
+                    $bg_url = $custom_bg ? (is_array($custom_bg) ? $custom_bg['url'] : wp_get_attachment_image_url($custom_bg, 'full')) : get_template_directory_uri() . '/images/delivery-background.jpg';
+                    ?>
+                    <div class="col-12 col-lg-4">
+                      <div class="<?php echo $bg_class; ?> position-relative delivery-section-height">
+                        <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
+                          <div class="p-3 p-lg-5">
+                            <a rel="nofollow" id="<?php the_sub_field('banner_id'); ?>"
+                              href="<?php the_sub_field('banner_link'); ?>"
+                              class="text-white stretched-link delivery-trigger" target="_blank">
+                              <div class="d-block">
+                                <?php $delivery_image = get_sub_field('delivery_image'); ?>
+                                <?php echo wp_get_attachment_image($delivery_image, "full", "", array("class" => "img-client")); ?>
+                              </div>
+                            </a>
+                          </div>
+                        </div>
+                        <img src="<?php echo $bg_url; ?>" class="img-cover">
+                      </div>
                     </div>
-                  </a>
-                </div>
+                  <?php endwhile; ?>
+                <?php endif; ?>
               </div>
-              <img src="https://www.goldenmonkeyubud.com/wp-content/uploads/2023/10/delivery-bg-grab.webp" class="img-cover">
-            </div>          
+            </div>
           </div>
-		 <!--Shopee Food>
-		  <div class="col-12 col-lg-4 g-1 g-lg-5">
-            <div class="bg-dark position-relative delivery-section-height">
-              <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
-                <div class="p-3 p-lg-5">
-                  <a rel="nofollow" id="shopee" href="https://shopee.co.id/universal-link/now-food/shop/20015132?deep_and_deferred=1&shareChannel=copy_link" class="text-white stretched-link delivery-trigger" target="_blank" >  
-                    <div class="d-block">
-                     <img src="https://www.goldenmonkeyubud.com/wp-content/uploads/2021/11/shopee-food-white-150.png" alt="Shopee Food" target="_blank" img-client> 
+
+          <!-- Sanur Tab -->
+          <div class="tab-pane fade" id="sanur" role="tabpanel" aria-labelledby="sanur-tab">
+            <div class="container">
+              <div class="row g-1 g-lg-5">
+                <?php if (have_rows('image_list_sanur')): ?>
+                  <?php while (have_rows('image_list_sanur')):
+                    the_row();
+                    $banner_id = get_sub_field('banner_id');
+                    $bg_class = 'bg-dark';
+                    if (stripos($banner_id, 'grab') !== false) {
+                      $bg_class = 'bg-grabfood';
+                    } elseif (stripos($banner_id, 'go') !== false) {
+                      $bg_class = 'bg-gofood';
+                    }
+
+                    // Get custom background image
+                    $custom_bg = get_sub_field('banner_background_image');
+                    $bg_url = $custom_bg ? (is_array($custom_bg) ? $custom_bg['url'] : wp_get_attachment_image_url($custom_bg, 'full')) : get_template_directory_uri() . '/images/delivery-background.jpg';
+                    ?>
+                    <div class="col-12 col-lg-4">
+                      <div class="<?php echo $bg_class; ?> position-relative delivery-section-height">
+                        <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
+                          <div class="p-3 p-lg-5">
+                            <a rel="nofollow" id="<?php the_sub_field('banner_id'); ?>"
+                              href="<?php the_sub_field('banner_link'); ?>"
+                              class="text-white stretched-link delivery-trigger" target="_blank">
+                              <div class="d-block">
+                                <?php $delivery_image = get_sub_field('delivery_image'); ?>
+                                <?php echo wp_get_attachment_image($delivery_image, "full", "", array("class" => "img-client")); ?>
+                              </div>
+                            </a>
+                          </div>
+                        </div>
+                        <img src="<?php echo $bg_url; ?>" class="img-cover">
+                      </div>
                     </div>
-                  </a>
-                </div>
+                  <?php endwhile; ?>
+                <?php endif; ?>
               </div>
-              <img src="https://www.goldenmonkeyubud.com/wp-content/uploads/2023/10/delivery-bg-shopee.webp" class="img-cover">
-            </div>          
+            </div>
           </div>
-		<!--Go Food-->
-		  <div class="col-12 col-lg-4 g-1 g-lg-5">
-            <div class="bg-dark position-relative delivery-section-height">
-              <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
-                <div class="p-3 p-lg-5">
-                  <a rel="nofollow" id="gofood" href="https://gofood.link/a/CtgSwQL" class="text-white stretched-link delivery-trigger" target="_blank" >  
-                    <div class="d-block">
-                     <img src="https://www.goldenmonkeyubud.com/wp-content/uploads/2021/11/gofood-150.png" alt="Go Food" target="_blank" img-client> 
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <img src="https://www.goldenmonkeyubud.com/wp-content/uploads/2023/10/delivery-bg-gojek.webp" class="img-cover">
-            </div>          
-          </div>
-        <!--<?php while ( have_rows('image_list') ) : the_row(); ?>
-          <div class="col-12 col-lg-4 g-1 g-lg-5">
-            <div class="bg-dark position-relative delivery-section-height">
-              <div class="d-flex h-100 align-items-center text-white featured-text justify-content-center">
-                <div class="p-3 p-lg-5">
-                  <a rel="nofollow" id="<?php the_sub_field('banner_id'); ?>" href="<?php the_sub_field('banner_link'); ?>" class="text-white stretched-link delivery-trigger" target="_blank" >  
-                    <div class="d-block">
-                    <?php $delivery_image = get_sub_field('delivery_image'); ?>
-                    <?php echo wp_get_attachment_image($delivery_image, "full", "", array( "class" => "img-client" )); ?> 
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <img src="<?php bloginfo('template_url'); ?>/images/delivery-background.jpg" class="img-cover">
-            </div>          
-          </div>
-          <?php endwhile; ?>-->
         </div>
       </div>
     </div>
-    <?php endif; ?>
 
-  </div> 
+  </div>
 </main>
-  
+
 <?php
 get_footer();
