1e7886d0470c5886df7ee23e2e63032a05eca346
Reza Firmansyah <erzethones.inc@gmail.com>
2026-03-17 10:05:06 +0800

    feat: Adjust padding for single post content and news section, and ensure text decoration remains none on hover for media cards.

--- single.php
+++ single.php
@@ -12,111 +12,111 @@ get_header();
 ?>
 
 <div class="bg-brand h-50">
-    <div class="container h-100">
-        <div class="row py-5 h-100 align-items-center">
-            <div class="col-12 py-3 py-lg-5 text-center text-white">
+	<div class="container h-100">
+		<div class="row py-5 h-100 align-items-center">
+			<div class="col-12 py-3 py-lg-5 text-center text-white">
 				<div class="mobilespacer d-block d-lg-none"></div>
-                <h1><?php the_title(); ?></h1>
-            </div>
-        </div>
-    </div>
+				<h1><?php the_title(); ?></h1>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="bg-white py-5">
 	<main id="primary" class="container py-3">
 		<div class="row gy-4 gx-lg-4">
-		<?php if ( have_posts() ) : 
-			while ( have_posts() ) :
-				the_post();
+			<?php if (have_posts()):
+				while (have_posts()):
+					the_post();
 
-				get_template_part( 'template-parts/content', 'single' );
+					get_template_part('template-parts/content', 'single');
 
-			endwhile;
-		
-		else :
+				endwhile;
 
-			get_template_part( 'template-parts/content', 'none' );
+			else:
 
-		endif;
-		?>
-		<div class="col-12 col-lg-4">
-			<div class="sticky-top recent-post">
-				<h4 class="normal-text fw-bold pb-4 px-5 px-lg-0">GOLDEN MONKEY NEWS</h4>
-				<?php
-				$args=array(
-					'orderby' => 'rand',
-					'post_type' => 'post',                          
-					'post_status' => 'publish',
-					'category_name' => 'news',
-					'posts_per_page' => 5                       
-				);
-				$the_pages = new WP_Query( $args );
+				get_template_part('template-parts/content', 'none');
 
-				if( $the_pages->have_posts() ){ ?>
-				<ul class="list-unstyled mb-0 px-5 px-lg-0">      
-				<?php
-					while( $the_pages->have_posts() ){
-						$the_pages->the_post();                                        
-						$size = 'thumbnail';
-					?>                				
-						<li class="pb-4 mb-4 row align-items-center">
-							<?php if ( has_post_thumbnail() ) : ?>
-							<div class="col-3 col-lg-4">
-								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-									<?php the_post_thumbnail($size, array( 'class' => 'img-fluid' )); ?>
-								</a>
-							</div>
-							<?php endif; ?>
-							<div class="col-9 col-lg-8">                        
-								<a class="black" href="<?php the_permalink(); ?>" class="fw-bold"><?php the_title(); ?></a>                     
-							</div>                        
-						</li>                
-				<?php
-					}
-				?>
-				</ul>
-				<?php            
-				}
-				wp_reset_query();
-				?>
-			</div>
-		</div>
-		<div class="col-12 col-lg-8 py-4">			
-			<div class="bg-light">
-				<nav id="nav-single" class="d-block d-lg-flex align-items-start">
+			endif;
+			?>
+			<div class="col-12 col-lg-4">
+				<div class="sticky-top recent-post">
+					<h4 class="normal-text fw-bold pb-4 px-2 px-lg-0">GOLDEN MONKEY NEWS</h4>
 					<?php
-						$prev_post = get_previous_post();
-						if ( ! empty( $prev_post ) ):
-							$id = $prev_post->ID ;
-							$permalink = get_permalink( $id );
-						endif;
+					$args = array(
+						'orderby' => 'rand',
+						'post_type' => 'post',
+						'post_status' => 'publish',
+						'category_name' => 'news',
+						'posts_per_page' => 5
+					);
+					$the_pages = new WP_Query($args);
+
+					if ($the_pages->have_posts()) { ?>
+						<ul class="list-unstyled mb-0 px-2 px-lg-0">
+							<?php
+							while ($the_pages->have_posts()) {
+								$the_pages->the_post();
+								$size = 'thumbnail';
+								?>
+								<li class="pb-4 mb-4 row align-items-center">
+									<?php if (has_post_thumbnail()): ?>
+										<div class="col-3 col-lg-4">
+											<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+												<?php the_post_thumbnail($size, array('class' => 'img-fluid')); ?>
+											</a>
+										</div>
+									<?php endif; ?>
+									<div class="col-9 col-lg-8">
+										<a class="black" href="<?php the_permalink(); ?>" class="fw-bold"><?php the_title(); ?></a>
+									</div>
+								</li>
+								<?php
+							}
+							?>
+						</ul>
+						<?php
+					}
+					wp_reset_query();
 					?>
-					<?php 
+				</div>
+			</div>
+			<div class="col-12 col-lg-8 py-4">
+				<div class="bg-light">
+					<nav id="nav-single" class="d-block d-lg-flex align-items-start">
+						<?php
+						$prev_post = get_previous_post();
+						if (!empty($prev_post)):
+							$id = $prev_post->ID;
+							$permalink = get_permalink($id);
+						endif;
+						?>
+						<?php
 						$next_post = get_next_post();
-						if ( ! empty( $next_post ) ):
-							$nid = $next_post->ID ;
+						if (!empty($next_post)):
+							$nid = $next_post->ID;
 							$permalink = get_permalink($nid);
 						endif;
-					?>
-					<span class="nav-previous d-block w-50 p-4 text-center text-lg-start">
-						<?php
-						if (! empty($prev_post)):
-						previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous<br>', 'goldenmonkey' ) );?> 
-						<strong><a href="<?php echo $permalink; ?>"><?php echo $prev_post->post_title; ?></a></strong>
-						<?php endif; ?>
-					</span>
-					<span class="nav-next d-block w-50 p-4 text-center text-lg-end">
-						<?php
-						if (! empty($next_post)):
-						next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span><br>', 'goldenmonkey' ) ); ?>
-						<strong><a href="<?php echo $permalink; ?>"><?php echo $next_post->post_title; ?></a></strong>
-						<?php endif; ?>
-					</span>
-				</nav>
+						?>
+						<span class="nav-previous d-block w-50 p-4 text-center text-lg-start">
+							<?php
+							if (!empty($prev_post)):
+								previous_post_link('%link', __('<span class="meta-nav">&larr;</span> Previous<br>', 'goldenmonkey')); ?>
+								<strong><a href="<?php echo $permalink; ?>"><?php echo $prev_post->post_title; ?></a></strong>
+							<?php endif; ?>
+						</span>
+						<span class="nav-next d-block w-50 p-4 text-center text-lg-end">
+							<?php
+							if (!empty($next_post)):
+								next_post_link('%link', __('Next <span class="meta-nav">&rarr;</span><br>', 'goldenmonkey')); ?>
+								<strong><a href="<?php echo $permalink; ?>"><?php echo $next_post->post_title; ?></a></strong>
+							<?php endif; ?>
+						</span>
+					</nav>
+				</div>
 			</div>
 		</div>
-		</div>
 	</main><!-- #main -->
-</div>	
+</div>
 
 <?php
 get_footer();
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,14 +10,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('col-12 col-lg-8 post-content'); ?>>
-	<div class="px-5 px-lg-0 pe-lg-5">
+    <div class="px-2 px-lg-0 pe-lg-5">
         <div class="text-center pb-4">
-        <?php if ( has_post_thumbnail() ) : ?>            
-            <?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>            
-        <?php endif; ?>
+            <?php if (has_post_thumbnail()): ?>
+                <?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
+            <?php endif; ?>
         </div>
-        <div class="entry-content text-start pb-5">		
+        <div class="entry-content text-start pb-5">
             <?php the_content(); ?>
-        </div><!-- .entry-content -->        
+        </div><!-- .entry-content -->
     </div>
 </article>
